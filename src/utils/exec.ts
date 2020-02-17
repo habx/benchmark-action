57a2a1c38ad372476e04b2c6a521728f5b1bf887
Jean Dessane <jean@habx.fr>
2020-02-17 11:38:49 +0100

    fix compilation

--- src/utils/exec.ts
+++ src/utils/exec.ts
@@ -3,6 +3,9 @@ const { exec: defaultExec } = require('child_process')
 const exec = (cmd: string) =>
   new Promise(resolve =>
     defaultExec(cmd, (error: string, stdout: string) => {
+      if (error) {
+        console.log(error) // eslint-disable-line
+      }
       return resolve(stdout)
     })
   )
